c5e56827c88166406ae0daa36ef25b8c4ea85528
karam fikre <karam_fikre@hotmail.com>
2018-02-15 15:15:46 +0100

    Add the Email sending and fixing the Design

--- MyWebPage/Controllers/HomeController.cs
+++ MyWebPage/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using MyWebPage.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,6 +17,25 @@ namespace MyWebPage.Controllers
             return View();
         }
 
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> SendEmail(ContactModel contactModel)
+        {
+            if (ModelState.IsValid) { 
+            var client = new SendGridClient("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");
+            var from = new EmailAddress(contactModel.Email, contactModel.UserName);
+            var subject = contactModel.Subject;
+            var to = new EmailAddress("shadow.walker@list.ru", "Shadow");
+            var plainTextContent = contactModel.Message;
+            var htmlContent = "<strong>"+contactModel.Message+"</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+
+            return View("Index");
+            }
+            return View("Index");
+        
+    }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ MyWebPage/Models/ContactModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyWebPage.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType("Email")]
+        public string  Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
